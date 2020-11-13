bc734585fe5f83c0c75b9ee81f5abf21a3fba928
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 05:01:24 -0800

    Connection à la base de données établie

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php include 'partials/header.php' ?>
+<?php include 'partials/header.php'; ?>
+<?php include 'lib/db.php'; ?>
   <div role="main" class="container">
 
     <h1>Mon portfolio en PHP</h1>
 
   </div>
 
-<?php include 'partials/footer.php' ?>
+<?php include 'partials/footer.php'; ?>
