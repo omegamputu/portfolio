8d0d9f6bd28c21d6f94707f88deb5b2aa40c6085
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 12:52:23 -0800

    la fonctionnalité de suppression d'une catégorie ajouteé

--- admin/category.php
+++ admin/category.php
@@ -2,6 +2,19 @@
 include '../lib/includes.php';
 include '../partials/admin_header.php';
 
+//
+if (isset($_GET['delete'])) {
+	# code...
+	checkCsrf();
+
+	$id = $db->quote($_GET['delete']);
+	$db->query("DELETE FROM categories WHERE id=$id");
+	setFlash("La catégorie a bien été supprimée."); 
+	header('location:category.php');
+	die();
+}
+
+// AFFICHAGE DES CATEGORIES
 $select = $db->query('SELECT id, name, slug FROM categories');
 $categories = $select->fetchAll(); 
 
@@ -10,6 +23,11 @@ $categories = $select->fetchAll();
 <div class="container">
 	<h2>Les catégories</h2>
 
+	<p>
+		<a href="category_edit.php" class="btn btn-secondary btn-sm">Ajouter une catégorie</a>
+	</p>
+
+	
 	<table class="table table-striped">
 		<thead>
 			<tr>
@@ -25,7 +43,7 @@ $categories = $select->fetchAll();
 					<td><?= $category['name']; ?></td>
 					<td>
 						<a href="category_edit.php?id=<?= $category['id']; ?>" class="btn btn-primary btn-sm">Editer</a>
-						<a href="?delete=<?= $category['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Etes-vous sûr de cette opération ?')">Supprimer</a>
+						<a href="?delete=<?= $category['id']; ?>&<?= csrf() ?>" class="btn btn-danger btn-sm" onclick="return confirm('Etes-vous sûr de cette opération ?')">Supprimer</a>
 					</td>
 				</tr>
 			<?php endforeach; ?>
