e9b184d99d3a466e72345066f6fb12c51c140001
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 08:53:08 -0800

    création de la partie d'administration

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+include '../lib/includes.php';
+include '../partials/admin_header.php';
+?>
+
+<div class="container">
+	<h2>Mon administration</h2>
+</div>
+
+<?php include '../partials/footer.php'; ?>
---
+++ partials/admin_header.php
@@ -0,0 +1,40 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Mon administration</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Tutoriel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Catégories</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+       <?= flash(); ?>
+    </div>
+
