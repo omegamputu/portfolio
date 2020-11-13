045e96db2afea9ee939889a8f66e125ced365eda
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 05:33:13 -0800

    le fichier index.php a subit un leger changement

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<?php include 'lib/db.php'; ?>
+<?php include 'lib/includes.php'; ?>
 <?php include 'partials/header.php'; ?>
   <div role="main" class="container">
 
     <h1>Mon portfolio en PHP</h1>
+
+    <?php include 'lib/debug.php'; ?>
 
   </div>
 
