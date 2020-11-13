9cbcae98f390ea3edae1bd21d240fbe636ebe450
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 06:54:17 -0800

    auth.php a subit un changement

--- lib/auth.php
+++ lib/auth.php
@@ -4,7 +4,7 @@ session_start();
 
 if (!isset($auth)) {
 	# code...
-	if (!isset($_SESSION['id'])) {
+	if (!isset($_SESSION['Auth']['id'])) {
 	# code...
 	header('location:' . WEBROOT . 'login.php');
    }
