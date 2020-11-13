d24c89faa04d5360cd46e46446d3c4f48d45d33e
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 09:42:17 -0800

    Le header de l'administration a été changer

--- partials/admin_header.php
+++ partials/admin_header.php
@@ -21,12 +21,12 @@
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Tutoriel</a>
+        <a class="navbar-brand" href="#">Administration</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a href="#" class="nav-link">Catégories</a>
           </li>
