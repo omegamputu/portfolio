4fa8c1b9a51b4465fc12e4e96516cfa675868be8
Omega Mputu <kutekemeni93@gmail.com>
2020-11-13 09:16:47 -0800

    Resolution d'un bug de l'appel du fichier css

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,8 @@ include '../partials/admin_header.php';
 
 <div class="container">
 	<h2>Mon administration</h2>
+
+	<?php include '../lib/debug.php'; ?>
 </div>
 
 <?php include '../partials/footer.php'; ?>
